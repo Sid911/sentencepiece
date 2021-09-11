272d50e98129edaf41145b22b4361ab136b236f8
Siddharth Sinha <siddharthsinha9752@gmail.com>
2021-09-11 22:00:02 +0530

    Added and verified encoder funciotnalities

--- src/sentencepiece_processor_c.cc
+++ src/sentencepiece_processor_c.cc
@@ -1,7 +1,4 @@
 #include "sentencepiece_processor_c.h"
-
-
-
 void* sentencepiceInit() {
 	return new sentencepiece::SentencePieceProcessor;
 }
@@ -20,13 +17,27 @@ void loadModelFile(void* processorhandle, char* filename) {
 	processor->LoadOrDie(filename);
 }
 
-int* encodeAsIds(void* processorhandle, char* input) {
+bool checkModelLoaded(void* processorhandle) { 
 	auto processor = static_cast<sentencepiece::SentencePieceProcessor*> (processorhandle);
-	auto result = processor->EncodeAsIds(input);
-	return result.get_allocator().allocate(result.size());
+	if (processor->status().code() == sentencepiece::util::StatusCode::kOk) {
+		return true;
+	}
+	return false;
+};
+
+void resetVocabulary(void* processorhandle) {
+	auto processor = static_cast<sentencepiece::SentencePieceProcessor*> (processorhandle);
+	processor->ResetVocabulary();
 }
 
-string_array encodeAsPieces(void* processorhandle, char* input) {
+struct Int32Array encodeAsIds(void* processorhandle, char* input) {
+	auto processor = static_cast<sentencepiece::SentencePieceProcessor*> (processorhandle);
+	auto result = processor->EncodeAsIds(input);
+	auto c_result = createInt32Array(result.data(), result.size());
+	return c_result;
+}
+
+struct string_array encodeAsPieces(void* processorhandle, char* input) {
 	auto processor = static_cast<sentencepiece::SentencePieceProcessor*> (processorhandle);
 	auto result =  processor->EncodeAsPieces(input);
 	std::vector<char*> cstrings{};
@@ -35,3 +46,7 @@ string_array encodeAsPieces(void* processorhandle, char* input) {
 
 	return createStringArray(cstrings.data(), cstrings.size());
 }
+
+//struct string_array decodeFromIds(void* processorhandle,int* ids) {
+//	//Todo: Remember to implement
+//}
--- src/sentencepiece_processor_c.h
+++ src/sentencepiece_processor_c.h
@@ -4,22 +4,35 @@
 struct string_array
 {
 	char** data;
-	unsigned int size;
+	int size;
+};
+struct Int32Array 
+{
+	int* data;
+	int len;
 };
 
-string_array createStringArray(char** cstring, unsigned int size) {
+string_array createStringArray(char** cstring, int size) {
 	string_array s;
 	s.data = cstring;
 	s.size = size;
 	return s;
 }
 
+Int32Array createInt32Array(int* arr, int len) {
+	Int32Array a;
+	a.data = arr;
+	a.len = len;
+	return a;
+}
+
 extern "C"{
 void* sentencepiceInit();
 void sentencepieceDestroy(void* processorhandle);
 void loadModelFile(void* processorhandle, char* filename);
-
-int* encodeAsIds(void* processorhandle, char* input);
-string_array encodeAsPieces(void* processorhandle, char* input);
+bool checkModelLoaded(void* processorhandle);
+void resetVocabulary(void* processorhandle);
+struct Int32Array encodeAsIds(void* processorhandle, char* input);
+struct string_array encodeAsPieces(void* processorhandle, char* input);
 
 }
